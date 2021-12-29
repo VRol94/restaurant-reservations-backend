78c7ac6d23e6dc16be5ac056baa56136853a9d5d
VRol94 <rolandvincze94@gmail.com>
2021-12-29 12:06:18 +0200

    Reservation deletion implemented.

--- constants.js
+++ constants.js
@@ -1,20 +1,20 @@
 exports.reservations = [
-    {
-      id: 0,
-      name: "Yoshi",
-      date: `${new Date().toDateString()} ${new Date().toLocaleTimeString(
-        "en-US"
-      )}`,
-      guestNr: 10,
-      email: "yoshi@gmail.com",
-    },
-    {
-      id: 1,
-      name: "Crystal",
-      date: `${new Date().toDateString()} ${new Date().toLocaleTimeString(
-        "en-US"
-      )}`,
-      guestNr: 50,
-      email: "crystal@yahoo.com",
-    },
-  ];
+  {
+    id: 1,
+    name: "Yoshi",
+    date: `${new Date().toDateString()} ${new Date().toLocaleTimeString(
+      "en-US"
+    )}`,
+    guestNr: 10,
+    email: "yoshi@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Crystal",
+    date: `${new Date().toDateString()} ${new Date().toLocaleTimeString(
+      "en-US"
+    )}`,
+    guestNr: 50,
+    email: "crystal@yahoo.com",
+  },
+];
--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require("express");
 const { RootMutationType } = require("./reservations-data/rootMutationType");
 const { RootQueryType } = require("./reservations-data/rootQueryType");
 const expressGraphQL = require("express-graphql").graphqlHTTP;
-const {
-  GraphQLSchema
-} = require("graphql");
+const { GraphQLSchema } = require("graphql");
+const cors = require("cors");
 const app = express();
 
 const schema = new GraphQLSchema({
@@ -12,6 +11,7 @@ const schema = new GraphQLSchema({
   mutation: RootMutationType,
 });
 
+app.use(cors());
 app.use(
   "/graphql",
   expressGraphQL({
